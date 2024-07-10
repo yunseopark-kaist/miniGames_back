afe9150348d0b57cc2348b4753d9e0c6fe06367a
seilj <been22026@gmail.com>
2024-07-10 16:17:19 +0900

    minor change

--- src/sharedgame/sharedgame.controller.ts
+++ src/sharedgame/sharedgame.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { SharedGameService } from './sharedgame.service';
 import { SharedGameDto } from './dto/save-sharedgame.dto';
 
@@ -10,6 +10,21 @@ export class SharedGameController {
   async shareGame(@Body() sharedGameDto: SharedGameDto) {
     return this.sharedGameService.shareGame(sharedGameDto);
   }
+
+  @Get(':userId')
+    async getSharedGames(
+        @Param('userId') userId: string
+    ): Promise<SharedGameDto[]> {
+        const sharedGames = await this.sharedGameService.getSharedGames(userId);
+
+        // SharedGameDto 형식으로 변환하여 반환
+        return sharedGames.map(game => ({
+            userId: game.userId,
+            shareduserId: game.shareduserId,
+            name: game.name,
+            gameState: game.gameState
+        }));
+    }
 
   // 필요에 따라 다른 엔드포인트 추가 가능
 }
